3e27555dd4478c46cefba16adb5fe19043e650fd
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-07-22 22:09:39 +0400

    setup gh-pages base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/softswiss_test_assignment',
   plugins: [react()],
   resolve: {
     alias: {
