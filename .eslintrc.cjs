6dc17775478e1c27972e461a27bed618eed8e431
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-07-22 20:41:14 +0400

    setup linters

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,13 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:css-modules/recommended',
+    'plugin:prettier/recommended', // should be last
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'css-modules', 'prettier'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-}
+};
