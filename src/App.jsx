1672c6b728a3c31eaf45c081316ce913cabb9e1e
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-07-23 18:32:28 +0400

    add desktop header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import BuscketIcon from '@assets/svg/busket.svg?react';
+
 function App() {
   return (
     <div className="page">
-      <p className="text">asdasdasdasdasdasd</p>
-
-      <header className="header"></header>
+      <header className="header">
+        <div className="header__content">
+          <div className="header__logo" />
+          <nav className="nav">
+            <ul className="nav__links">
+              <li>
+                <a href="#" className="nav__link">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#" className="nav__link">
+                  Products
+                </a>
+              </li>
+              <li>
+                <a href="#" className="nav__link">
+                  <BuscketIcon className="nav__icon" />
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
       <main></main>
       <footer className="footer"></footer>
     </div>
