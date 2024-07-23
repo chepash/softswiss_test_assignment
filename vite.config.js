1c2364c19228bb6ac0ec10fa0bd934e0cc7b046a
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-07-23 18:30:57 +0400

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
   base: '/softswiss_test_assignment',
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       '@assets': resolve(__dirname, 'src/assets'),
