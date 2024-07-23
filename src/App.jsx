0232e085fdb58bdcd725618aebad0f3e1cdf90f5
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-07-23 22:05:29 +0400

    add promo section

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,21 @@ function App() {
           </nav>
         </div>
       </header>
-      <main className="page__content"></main>
+      <main className="page__content">
+        <section className="promo" aria-label="promo section">
+          <div className="promo__content">
+            <h1 className="promo__title">
+              Discover the vast expanses of <span className="promo__title_pink">space</span>
+            </h1>
+            <p className="promo__subtitle">
+              Where the possibilities are <span className="promo__subtitle_yellow">endless!</span>
+            </p>
+            <button className="button">Learn more</button>
+          </div>
+        </section>
+        <section aria-label="Offers cards section"></section>
+        <section aria-label="Embark on a space journey article section"></section>
+      </main>
       <footer className="footer page__footer">
         <RocketIcon className="footer__icon" />
         <p className="footer__text">Exciting space adventure!</p>
